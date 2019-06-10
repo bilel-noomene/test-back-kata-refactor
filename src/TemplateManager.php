718820b5caab58a1ee2c64fb1b2a3887de8eb5f9
Bilel Noômene <bilel.noomene@gmail.com>
2019-06-10 10:36:12 +0200

    Simplify TemplateManager

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -12,13 +12,9 @@ use App\Repository\SiteRepository;
 
 class TemplateManager
 {
-    public function getTemplateComputed(Template $tpl, array $data)
+    public function getTemplateComputed(Template $template, array $data)
     {
-        if (!$tpl) {
-            throw new \RuntimeException('no tpl given');
-        }
-
-        $replaced = clone($tpl);
+        $replaced = clone($template);
         $replaced->subject = $this->computeText($replaced->subject, $data);
         $replaced->content = $this->computeText($replaced->content, $data);
 
@@ -29,55 +25,57 @@ class TemplateManager
     {
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
+        $quote = (isset($data['quote']) && $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
-        if ($quote)
-        {
+        if ($quote) {
             $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $site = SiteRepository::getInstance()->getById($quote->siteId);
+            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+            if ($this->hasPlaceholder($text, '[quote:destination_link]') && $destination) {
+                $destinationLink = sprintf('%s/%s/quote/%d', $site->url, $destination->countryName, $_quoteFromRepository->id);
+                $text = str_replace('[quote:destination_link]', $destinationLink, $text);
             }
 
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
+            if ($this->hasPlaceholder($text, '[quote:summary_html]')) {
+                $text = str_replace('[quote:summary_html]', Quote::renderHtml($_quoteFromRepository), $text);
             }
 
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+            if ($this->hasPlaceholder($text, '[quote:summary]')) {
+                $text = str_replace('[quote:summary]', Quote::renderText($_quoteFromRepository), $text);
+            }
+
+            if ($this->hasPlaceholder($text, '[quote:destination_name]')) {
+                // There is no check that $destination is not null as in the case of '[quote:destination_link]'
+                $text = str_replace('[quote:destination_name]', $destination->countryName, $text);
+            }
         }
 
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
+        if (!isset($destination)) {
+            // Considering that $destination is merged with $destinationOfQuote, the previous (!isset($destination)) is
+            //  now equal to (!($quote && $this->hasPlaceholder($text, '[quote:destination_link]') && isset($destination)))
+            //  or (!$quote || !$this->hasPlaceholder($text, '[quote:destination_link]') || !isset($destination)))).
+            // Because replacing '[quote:destination_link]' when (!$this->hasTag($text, '[quote:destination_link]')) has
+            //  no effect and  (!isset($destination)) is true when (!$quote) is true, the condition is simplified to
+            //  !isset($destination).
             $text = str_replace('[quote:destination_link]', '', $text);
+        }
 
         /*
          * USER
          * [user:*]
          */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
-        if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
+        $_user = (isset($data['user']) && ($data['user'] instanceof User)) ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
+
+        if ($_user && $this->hasPlaceholder($text, '[user:first_name]')) {
+            $text = str_replace('[user:first_name]', ucfirst(mb_strtolower($_user->firstname)), $text);
         }
 
         return $text;
+    }
+
+    private function hasPlaceholder($text, $placeholder)
+    {
+        return strpos($text, $placeholder) !== false;
     }
 }
