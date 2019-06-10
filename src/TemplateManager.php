ffd684c99df8e4d335f14ed905f808d558d60b48
Bilel Noômene <bilel.noomene@gmail.com>
2019-06-10 10:37:04 +0200

    Optimize TemplateManager

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -7,7 +7,6 @@ use App\Entity\Quote;
 use App\Entity\Template;
 use App\Entity\User;
 use App\Repository\DestinationRepository;
-use App\Repository\QuoteRepository;
 use App\Repository\SiteRepository;
 
 class TemplateManager
@@ -23,59 +22,33 @@ class TemplateManager
 
     private function computeText($text, array $data)
     {
-        $APPLICATION_CONTEXT = ApplicationContext::getInstance();
-
         $quote = (isset($data['quote']) && $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
-        if ($quote) {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
+        if ($quote && $destination = DestinationRepository::getInstance()->getById($quote->destinationId)) {
             $site = SiteRepository::getInstance()->getById($quote->siteId);
-            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $destinationLink = sprintf('%s/%s/quote/%d', $site->url, $destination->countryName, $quote->id);
 
-            if ($this->hasPlaceholder($text, '[quote:destination_link]') && $destination) {
-                $destinationLink = sprintf('%s/%s/quote/%d', $site->url, $destination->countryName, $_quoteFromRepository->id);
-                $text = str_replace('[quote:destination_link]', $destinationLink, $text);
-            }
-
-            if ($this->hasPlaceholder($text, '[quote:summary_html]')) {
-                $text = str_replace('[quote:summary_html]', Quote::renderHtml($_quoteFromRepository), $text);
-            }
-
-            if ($this->hasPlaceholder($text, '[quote:summary]')) {
-                $text = str_replace('[quote:summary]', Quote::renderText($_quoteFromRepository), $text);
-            }
-
-            if ($this->hasPlaceholder($text, '[quote:destination_name]')) {
-                // There is no check that $destination is not null as in the case of '[quote:destination_link]'
-                $text = str_replace('[quote:destination_name]', $destination->countryName, $text);
-            }
+            $text = str_replace('[quote:destination_link]', $destinationLink, $text);
+            $text = str_replace('[quote:destination_name]', $destination->countryName, $text);
+        } else {
+            $text = str_replace('[quote:destination_link]', '', $text);
         }
 
-        if (!isset($destination)) {
-            // Considering that $destination is merged with $destinationOfQuote, the previous (!isset($destination)) is
-            //  now equal to (!($quote && $this->hasPlaceholder($text, '[quote:destination_link]') && isset($destination)))
-            //  or (!$quote || !$this->hasPlaceholder($text, '[quote:destination_link]') || !isset($destination)))).
-            // Because replacing '[quote:destination_link]' when (!$this->hasTag($text, '[quote:destination_link]')) has
-            //  no effect and  (!isset($destination)) is true when (!$quote) is true, the condition is simplified to
-            //  !isset($destination).
-            $text = str_replace('[quote:destination_link]', '', $text);
+        if ($quote) {
+            $text = str_replace('[quote:summary_html]', Quote::renderHtml($quote), $text);
+            $text = str_replace('[quote:summary]', Quote::renderText($quote), $text);
         }
 
         /*
          * USER
          * [user:*]
          */
-        $_user = (isset($data['user']) && ($data['user'] instanceof User)) ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
+        $_user = (isset($data['user']) && $data['user'] instanceof User) ? $data['user'] : ApplicationContext::getInstance()->getCurrentUser();
 
-        if ($_user && $this->hasPlaceholder($text, '[user:first_name]')) {
+        if ($_user) {
             $text = str_replace('[user:first_name]', ucfirst(mb_strtolower($_user->firstname)), $text);
         }
 
         return $text;
-    }
-
-    private function hasPlaceholder($text, $placeholder)
-    {
-        return strpos($text, $placeholder) !== false;
     }
 }
