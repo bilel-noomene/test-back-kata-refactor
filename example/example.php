21b81b17f995d7cce5ff143e28d08170e162de21
Bilel Noômene <bilel.noomene@gmail.com>
2019-06-10 10:35:31 +0200

    Using namespaces

--- example/example.php
+++ example/example.php
@@ -1,23 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__).'/bootstrap.php';
 
-require_once __DIR__ . '/../src/Entity/Destination.php';
-require_once __DIR__ . '/../src/Entity/Quote.php';
-require_once __DIR__ . '/../src/Entity/Site.php';
-require_once __DIR__ . '/../src/Entity/Template.php';
-require_once __DIR__ . '/../src/Entity/User.php';
-require_once __DIR__ . '/../src/Helper/SingletonTrait.php';
-require_once __DIR__ . '/../src/Context/ApplicationContext.php';
-require_once __DIR__ . '/../src/Repository/Repository.php';
-require_once __DIR__ . '/../src/Repository/DestinationRepository.php';
-require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
-require_once __DIR__ . '/../src/Repository/SiteRepository.php';
-require_once __DIR__ . '/../src/TemplateManager.php';
+use App\Entity\Quote;
+use App\Entity\Template;
+use App\TemplateManager;
+use Faker\Factory;
 
-$faker = \Faker\Factory::create();
+$faker = Factory::create();
 
-$template = new Template(
+$template = new  Template(
     1,
     'Votre voyage avec une agence locale [quote:destination_name]',
     "
@@ -30,6 +22,7 @@ Bien cordialement,
 L'équipe Evaneos.com
 www.evaneos.com
 ");
+
 $templateManager = new TemplateManager();
 
 $message = $templateManager->getTemplateComputed(
